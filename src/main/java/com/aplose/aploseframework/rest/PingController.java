f6cd1950c9eb99e9ff2c191eb7fd5f2c476d2831
aplose <oandrade@aplose.fr>
2024-08-06 11:21:23 +0200

    Add crossorigin

--- src/main/java/com/aplose/aploseframework/rest/PingController.java
+++ src/main/java/com/aplose/aploseframework/rest/PingController.java
@@ -1,5 +1,6 @@
 package com.aplose.aploseframework.rest;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author oandrade
  */
 @RestController
+@CrossOrigin
 @RequestMapping(path = "/api")
 public class PingController {
     @GetMapping("/ping")
--- src/main/java/com/aplose/aploseframework/rest/TranslationController.java
+++ src/main/java/com/aplose/aploseframework/rest/TranslationController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Locale;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author oandrade
  */
 @RestController
+@CrossOrigin
 @RequestMapping("/api/translation")
 public class TranslationController {
     @Autowired
