4f23de002e2d7293b8265b1beb2a9199585858be
Florian Tocco <t.florian181181@gmail.com>
2024-09-10 18:50:07 +0200

    [REFACTO] deplacement des propriétés de secret.properties vers ConfigService et aploseframework.properties

--- src/main/java/com/aplose/aploseframework/service/ConfigService.java
+++ src/main/java/com/aplose/aploseframework/service/ConfigService.java
@@ -9,9 +9,12 @@ import com.aplose.aploseframework.repository.ConfigRepository;
 import jakarta.annotation.PostConstruct;
 import java.time.Instant;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -43,135 +46,276 @@ public class ConfigService {
     private String superAdminDefaultPassword;
     @Value("${google.client.id}")
     private String googleClientId;
+    @Value("vizulive.server.url")
+    private String vizuliveServerUrl;
+    @Value("vizulive.admin.id")
+    private String vizuliveAdminId;
+    @Value("vizulive.admin.user")
+    private String vizuliveAdminUser;
+    @Value("vizulive.admin.password")
+    private String vizuliveAdminPassword;
+    @Value("spring.mail.username")
+    private String springMailUsername;
+    @Value("spring.mail.password")
+    private String springMailPassword;
     
     private Map<String,Config> configurations = new HashMap<>();
     
     
     @PostConstruct    
     private void init(){
-        List<String> allKeys = configRepository.getAllKeys();
-        //on va voir si les config existent sinon on les crée à la volée...
-        Optional<Config> configOptional;
-        Config config=null;
+        // List<String> allKeys = configRepository.getAllKeys();
+        // //on va voir si les config existent sinon on les crée à la volée...
+        // Optional<Config> configOptional;
+        // Config config=null;
 
         // temps de validité, en seconde, du code d'activation de compte lors d'un enregistrement d'un utilisateur 
-        configOptional = configRepository.findById("spring.user-account.second-to-activate-account");
-        if(configOptional.isEmpty()){
-            config = new Config("spring.user-account.second-to-activate-account", secondToActivateAccount);
-            configRepository.save(config);
-            allKeys.remove(config.getConfigKey());
-            configurations.put(config.getConfigKey(), config);
-        }else{
-            allKeys.remove(configOptional.get().getConfigKey());
-        }
+        this.setConfig("spring.user-account.second-to-activate-account", secondToActivateAccount);
+
         // mot de passe par defaut du super admin
-        configOptional = configRepository.findById("aplose.framework.superAdmin.defaultPassword");
-        if(configOptional.isEmpty()){
-            config = new Config("aplose.framework.superAdmin.defaultPassword", superAdminDefaultPassword);
-            configRepository.save(config);
-            allKeys.remove(config.getConfigKey());
-            configurations.put(config.getConfigKey(), config);
-        }else{
-            allKeys.remove(configOptional.get().getConfigKey());
-        }
+        this.setConfig("aplose.framework.superAdmin.defaultPassword", superAdminDefaultPassword);
+
         // phrase secrete pour le Json Web Token d'authentification
-        configOptional = configRepository.findById("aplose.framework.security.jwt.secretKey");
-        if(configOptional.isEmpty()){
-            config = new Config("aplose.framework.security.jwt.secretKey", jwtSecretKey);
-            configRepository.save(config);
-            allKeys.remove(config.getConfigKey());
-            configurations.put(config.getConfigKey(), config);
-        }else{
-            allKeys.remove(configOptional.get().getConfigKey());
-        }
+        this.setConfig("aplose.framework.security.jwt.secretKey", jwtSecretKey);
+
         //stripe api key
-        configOptional = configRepository.findById("stripe.api.key");
-        if(configOptional.isEmpty()){
-            config = new Config("stripe.api.key", stripeApiKey);
-            configRepository.save(config);
-            allKeys.remove(config.getConfigKey());
-            configurations.put(config.getConfigKey(), config);
-        }else{
-            allKeys.remove(configOptional.get().getConfigKey());
-        }
+        this.setConfig("stripe.api.key", stripeApiKey);
 
         //stripe webhook secret
-        configOptional = configRepository.findById("stripe.webhook.secret");
-        System.err.println("ok");
-        if(configOptional.isEmpty()){
-            config = new Config("stripe.webhook.secret", stripeWebHookSecret);
-            configRepository.save(config);
-            allKeys.remove(config.getConfigKey());
-            configurations.put(config.getConfigKey(), config);
-        }else{
-            allKeys.remove(configOptional.get().getConfigKey());
-        }
+        this.setConfig("stripe.webhook.secret", stripeWebHookSecret);
 
-        //google.client.id utilisé pour l'authentification grâce à Google
-        configOptional = configRepository.findById("google.client.id");
-        if(configOptional.isEmpty()){
-            config = new Config("google.client.id", googleClientId);
-            configRepository.save(config);
-            allKeys.remove(config.getConfigKey());
-            configurations.put(config.getConfigKey(), config);
-        }else{
-            allKeys.remove(configOptional.get().getConfigKey());
-        }
+        // google.client.id utilisé pour l'authentification grâce à Google
+        this.setConfig("google.client.id", googleClientId);
+
         //dolibarr.api.url utilisé pour accéder à Dolibarr
         //on utilise le dolibarr serenitydate par défaut
-        configOptional = configRepository.findById("dolibarr.api.url");
-        if(configOptional.isEmpty()){
-            config = new Config("dolibarr.api.url", dolibarrApiUrl);
-            configRepository.save(config);
-            allKeys.remove(config.getConfigKey());
-            configurations.put(config.getConfigKey(), config);
-        }else{
-            allKeys.remove(configOptional.get().getConfigKey());
-        }
+        this.setConfig("dolibarr.api.url", dolibarrApiUrl);
+
         //dolibarr.user.api.key utilisé pour accéder à Dolibarr
         //on utilise le dolibarr serenitydate par défaut
-        configOptional = configRepository.findById("dolibarr.user.api.key");
-        if(configOptional.isEmpty()){
-            config = new Config("dolibarr.user.api.key", dolibarrUserApiKey);
-            configRepository.save(config);
-            allKeys.remove(config.getConfigKey());
-            configurations.put(config.getConfigKey(), config);
-        }else{
-            allKeys.remove(configOptional.get().getConfigKey());
-        }
+        this.setConfig("dolibarr.user.api.key", dolibarrUserApiKey);
+
         //appRootUrl utilisé pour les liens stripe
-        configOptional = configRepository.findById("app.root.url");
-        if(configOptional.isEmpty()){
-            config = new Config("app.root.url", appRootUrl);
-            configRepository.save(config);
-            allKeys.remove(config.getConfigKey());
-            configurations.put(config.getConfigKey(), config);
-        }else{
-            allKeys.remove(configOptional.get().getConfigKey());
-        }
+        this.setConfig("app.root.url", appRootUrl);
+
         //aploseframework.backend.root.url utilisé pour les liens envoyés par email
-        configOptional = configRepository.findById("aploseframework.backend.root.url");
-        if(configOptional.isEmpty()){
-            config = new Config("aploseframework.backend.root.url", "http://localhost:8087");        
-            configRepository.save(config);
-            allKeys.remove(config.getConfigKey());
-            configurations.put(config.getConfigKey(), config);
-        }else{
-            allKeys.remove(configOptional.get().getConfigKey());
-        }
+        this.setConfig("aploseframework.backend.root.url", "http://localhost:8087");
+        
         //aploseframework.mail.fromEmail utilisé pour les envoi d'email
-        configOptional = configRepository.findById("aploseframework.mail.fromEmail");
-        if(configOptional.isEmpty()){
-            config = new Config("aploseframework.mail.fromEmail", "noreply@aplose.fr");
-            configRepository.save(config);
-            allKeys.remove(config.getConfigKey());
-            configurations.put(config.getConfigKey(), config);
-        }else{
-            allKeys.remove(configOptional.get().getConfigKey());
-        }
-        // for(String keyToDelete:allKeys){
-        //     configRepository.deleteById(keyToDelete);
+        this.setConfig("aploseframework.mail.fromEmail", "noreply@aplose.fr");
+        
+        this.setConfig("vizulive.server.url", vizuliveServerUrl);
+        this.setConfig("vizulive.admin.id", vizuliveAdminId);
+        this.setConfig("vizulive.admin.user", vizuliveAdminUser);
+        this.setConfig("vizulive.admin.password", vizuliveAdminPassword);
+
+        this.setConfig("spring.mail.username", springMailUsername);
+        this.setConfig("spring.mail.password", springMailPassword);
+
+
+
+        // // temps de validité, en seconde, du code d'activation de compte lors d'un enregistrement d'un utilisateur 
+        // configOptional = configRepository.findById("spring.user-account.second-to-activate-account");
+        // if(configOptional.isEmpty()){
+        //     config = new Config("spring.user-account.second-to-activate-account", secondToActivateAccount);
+        //     configRepository.save(config);
+        //     allKeys.remove(config.getConfigKey());
+        //     configurations.put(config.getConfigKey(), config);
+        // }else{
+        //     allKeys.remove(configOptional.get().getConfigKey());
         // }
+        // // mot de passe par defaut du super admin
+        // configOptional = configRepository.findById("aplose.framework.superAdmin.defaultPassword");
+        // if(configOptional.isEmpty()){
+        //     config = new Config("aplose.framework.superAdmin.defaultPassword", superAdminDefaultPassword);
+        //     configRepository.save(config);
+        //     allKeys.remove(config.getConfigKey());
+        //     configurations.put(config.getConfigKey(), config);
+        // }else{
+        //     allKeys.remove(configOptional.get().getConfigKey());
+        // }
+        // // phrase secrete pour le Json Web Token d'authentification
+        // configOptional = configRepository.findById("aplose.framework.security.jwt.secretKey");
+        // if(configOptional.isEmpty()){
+        //     config = new Config("aplose.framework.security.jwt.secretKey", jwtSecretKey);
+        //     configRepository.save(config);
+        //     allKeys.remove(config.getConfigKey());
+        //     configurations.put(config.getConfigKey(), config);
+        // }else{
+        //     allKeys.remove(configOptional.get().getConfigKey());
+        // }
+        // //stripe api key
+        // configOptional = configRepository.findById("stripe.api.key");
+        // if(configOptional.isEmpty()){
+        //     config = new Config("stripe.api.key", stripeApiKey);
+        //     configRepository.save(config);
+        //     allKeys.remove(config.getConfigKey());
+        //     configurations.put(config.getConfigKey(), config);
+        // }else{
+        //     allKeys.remove(configOptional.get().getConfigKey());
+        // }
+
+        // //stripe webhook secret
+        // configOptional = configRepository.findById("stripe.webhook.secret");
+        // System.err.println("ok");
+        // if(configOptional.isEmpty()){
+        //     config = new Config("stripe.webhook.secret", stripeWebHookSecret);
+        //     configRepository.save(config);
+        //     allKeys.remove(config.getConfigKey());
+        //     configurations.put(config.getConfigKey(), config);
+        // }else{
+        //     allKeys.remove(configOptional.get().getConfigKey());
+        // }
+
+        // //google.client.id utilisé pour l'authentification grâce à Google
+        // configOptional = configRepository.findById("google.client.id");
+        // if(configOptional.isEmpty()){
+        //     config = new Config("google.client.id", googleClientId);
+        //     configRepository.save(config);
+        //     allKeys.remove(config.getConfigKey());
+        //     configurations.put(config.getConfigKey(), config);
+        // }else{
+        //     allKeys.remove(configOptional.get().getConfigKey());
+        // }
+        // //dolibarr.api.url utilisé pour accéder à Dolibarr
+        // //on utilise le dolibarr serenitydate par défaut
+        // configOptional = configRepository.findById("dolibarr.api.url");
+        // if(configOptional.isEmpty()){
+        //     config = new Config("dolibarr.api.url", dolibarrApiUrl);
+        //     configRepository.save(config);
+        //     allKeys.remove(config.getConfigKey());
+        //     configurations.put(config.getConfigKey(), config);
+        // }else{
+        //     allKeys.remove(configOptional.get().getConfigKey());
+        // }
+        // //dolibarr.user.api.key utilisé pour accéder à Dolibarr
+        // //on utilise le dolibarr serenitydate par défaut
+        // configOptional = configRepository.findById("dolibarr.user.api.key");
+        // if(configOptional.isEmpty()){
+        //     config = new Config("dolibarr.user.api.key", dolibarrUserApiKey);
+        //     configRepository.save(config);
+        //     allKeys.remove(config.getConfigKey());
+        //     configurations.put(config.getConfigKey(), config);
+        // }else{
+        //     allKeys.remove(configOptional.get().getConfigKey());
+        // }
+        // //appRootUrl utilisé pour les liens stripe
+        // configOptional = configRepository.findById("app.root.url");
+        // if(configOptional.isEmpty()){
+        //     config = new Config("app.root.url", appRootUrl);
+        //     configRepository.save(config);
+        //     allKeys.remove(config.getConfigKey());
+        //     configurations.put(config.getConfigKey(), config);
+        // }else{
+        //     allKeys.remove(configOptional.get().getConfigKey());
+        // }
+        // //aploseframework.backend.root.url utilisé pour les liens envoyés par email
+        // configOptional = configRepository.findById("aploseframework.backend.root.url");
+        // if(configOptional.isEmpty()){
+        //     config = new Config("aploseframework.backend.root.url", "http://localhost:8087");        
+        //     configRepository.save(config);
+        //     allKeys.remove(config.getConfigKey());
+        //     configurations.put(config.getConfigKey(), config);
+        // }else{
+        //     allKeys.remove(configOptional.get().getConfigKey());
+        // }
+        // //aploseframework.mail.fromEmail utilisé pour les envoi d'email
+        // configOptional = configRepository.findById("aploseframework.mail.fromEmail");
+        // if(configOptional.isEmpty()){
+        //     config = new Config("aploseframework.mail.fromEmail", "noreply@aplose.fr");
+        //     configRepository.save(config);
+        //     allKeys.remove(config.getConfigKey());
+        //     configurations.put(config.getConfigKey(), config);
+        // }else{
+        //     allKeys.remove(configOptional.get().getConfigKey());
+        // }
+        // // for(String keyToDelete:allKeys){
+        // //     configRepository.deleteById(keyToDelete);
+        // // }
+    }
+
+    private void setConfig(String key, String value){
+        Optional<Config> configOptional = configRepository.findById(key);
+        if(configOptional.isEmpty()){
+            Config config = new Config(key, value);
+            configRepository.save(config);
+            // allKeys.remove(config.getConfigKey());
+            this.configurations.put(config.getConfigKey(), config);
+        }else{
+            // allKeys.remove(configOptional.get().getConfigKey());
+        }
+    }
+
+    private void setConfig(String key, Long value){
+        Optional<Config> configOptional = configRepository.findById(key);
+        if(configOptional.isEmpty()){
+            Config config = new Config(key, value);
+            configRepository.save(config);
+            // allKeys.remove(config.getConfigKey());
+            this.configurations.put(config.getConfigKey(), config);
+        }else{
+            // allKeys.remove(configOptional.get().getConfigKey());
+        }
+    }
+
+    private void setConfig(String key, Boolean value){
+        Optional<Config> configOptional = configRepository.findById(key);
+        if(configOptional.isEmpty()){
+            Config config = new Config(key, value);
+            configRepository.save(config);
+            // allKeys.remove(config.getConfigKey());
+            this.configurations.put(config.getConfigKey(), config);
+        }else{
+            // allKeys.remove(configOptional.get().getConfigKey());
+        }
+    }
+
+    private void setConfig(String key, Integer value){
+        Optional<Config> configOptional = configRepository.findById(key);
+        if(configOptional.isEmpty()){
+            Config config = new Config(key, value);
+            configRepository.save(config);
+            // allKeys.remove(config.getConfigKey());
+            this.configurations.put(config.getConfigKey(), config);
+        }else{
+            // allKeys.remove(configOptional.get().getConfigKey());
+        }
+    }
+
+    private void setConfig(String key, Float value){
+        Optional<Config> configOptional = configRepository.findById(key);
+        if(configOptional.isEmpty()){
+            Config config = new Config(key, value);
+            configRepository.save(config);
+            // allKeys.remove(config.getConfigKey());
+            this.configurations.put(config.getConfigKey(), config);
+        }else{
+            // allKeys.remove(configOptional.get().getConfigKey());
+        }
+    }
+
+    private void setConfig(String key, Instant value){
+        Optional<Config> configOptional = configRepository.findById(key);
+        if(configOptional.isEmpty()){
+            Config config = new Config(key, value);
+            configRepository.save(config);
+            // allKeys.remove(config.getConfigKey());
+            this.configurations.put(config.getConfigKey(), config);
+        }else{
+            // allKeys.remove(configOptional.get().getConfigKey());
+        }
+    }
+
+    private void setConfig(String key, Double value){
+        Optional<Config> configOptional = configRepository.findById(key);
+        if(configOptional.isEmpty()){
+            Config config = new Config(key, value);
+            configRepository.save(config);
+            // allKeys.remove(config.getConfigKey());
+            this.configurations.put(config.getConfigKey(), config);
+        }else{
+            // allKeys.remove(configOptional.get().getConfigKey());
+        }
     }
 
     public String getStringConfig(String key){
